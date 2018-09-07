a0aff16cb1e79a834429cdef3d667d43e9d4903f
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-09-07 14:03:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
+let winOrNot = 0;
+function init(e) {
   // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function(e){
+    const keyPressed = parseInt(e.detail || e.which);
 
+    if(keyPressed === code[winOrNot]){
+      winOrNot++
+
+      if(winOrNot === code.length){
+        alert("You did it!!");
+
+        winOrNot = 0;
+      }
+
+    }else {
+
+      winOrNot = 0;
+    }
+  });
 }
